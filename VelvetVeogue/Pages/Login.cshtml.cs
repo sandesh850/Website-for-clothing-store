feb86440b3bf8037856ac6239bf90b4a5726e098
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-02 22:18:08 +0530

    initializing login page (step 01)

---
+++ VelvetVeogue/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VelvetVeogue.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

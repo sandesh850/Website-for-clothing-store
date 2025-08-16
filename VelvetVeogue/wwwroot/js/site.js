f69caadff24dc547ec8e50ca6a36d3001decfcbb
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-16 11:04:30 +0530

    Inserting Placeholder attribute

--- VelvetVeogue/wwwroot/js/site.js
+++ VelvetVeogue/wwwroot/js/site.js
@@ -5,21 +5,153 @@
 
 // Add Items page
 
+///
+/// No need the following code. Because, we can use placeholder attribute to do the following function. Placeholder
+/// attribute is buildin feature in HTML
+///
+
 // This code is used to clear and retype the text boxes value (hint | ex: label- category code, text box - category  code)
-$("#valueChanging").click(
+// Category code
+//$("#valueChanging01").click(
 
-    function () {
+//    function () {
 
-        $("#valueChanging").val("");
-    }
+//        $("#valueChanging01").val("");
+//    }
 
-);
+//);
 
-$("#valueChanging").mouseleave(
+//$("#valueChanging01").mouseleave(
 
-    function () {
+//    function () {
 
-        $("#valueChanging").val("Category code");
-    }
+//        var existing_val = "";
+//        existing_val = $("#valueChanging01").val();
 
-);
+//        if (existing_val === "" ) {
+
+//            $("#valueChanging01").val("Category code");
+//        }
+
+       
+//    }
+
+//);
+
+//// Category name
+//$("#valueChanging02").click(
+
+//    function () {
+
+//        $("#valueChanging02").val("");
+//    }
+
+//);
+
+//$("#valueChanging02").mouseleave(
+
+//    function () {
+
+//        $("#valueChanging02").val("Category Name");
+//    }
+
+//);
+
+//// Name
+//$("#valueChanging03").click(
+
+//    function () {
+
+//        $("#valueChanging03").val("");
+//    }
+
+//);
+
+//$("#valueChanging03").mouseleave(
+
+//    function () {
+
+//        $("#valueChanging03").val("Name");
+//    }
+
+//);
+
+
+//// Color
+//$("#valueChanging04").click(
+
+//    function () {
+
+//        $("#valueChanging04").val("");
+//    }
+
+//);
+
+//$("#valueChanging04").mouseleave(
+
+//    function () {
+
+//        $("#valueChanging04").val("Color");
+//    }
+
+//);
+
+
+//// Size
+//$("#valueChanging05").click(
+
+//    function () {
+
+//        $("#valueChanging05").val("");
+//    }
+
+//);
+
+//$("#valueChanging05").mouseleave(
+
+//    function () {
+
+//        $("#valueChanging05").val("Size");
+//    }
+
+//);
+
+
+//// Available Qty
+//$("#valueChanging06").click(
+
+//    function () {
+
+//        $("#valueChanging06").val("");
+//    }
+
+//);
+
+//$("#valueChanging06").mouseleave(
+
+//    function () {
+
+//        $("#valueChanging06").val("Available Qty");
+//    }
+
+//);
+
+
+//// Price
+//$("#valueChanging07").click(
+
+//    function () {
+
+//        $("#valueChanging07").val("");
+//    }
+
+//);
+
+//$("#valueChanging07").mouseleave(
+
+//    function () {
+
+//        $("#valueChanging07").val("Price");
+//    }
+
+//);

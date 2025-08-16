343e7b60de484b7a16f28475397b079d2cb190eb
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-16 10:03:54 +0530

    Intializing JavaScript functions | status: Not completed | Page: Add items page

--- VelvetVeogue/wwwroot/js/site.js
+++ VelvetVeogue/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Add Items page
+
+// This code is used to clear and retype the text boxes value (hint | ex: label- category code, text box - category  code)
+$("#valueChanging").click(
+
+    function () {
+
+        $("#valueChanging").val("");
+    }
+
+);
+
+$("#valueChanging").mouseleave(
+
+    function () {
+
+        $("#valueChanging").val("Category code");
+    }
+
+);

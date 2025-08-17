3763a8d035fdbf851e3d6429e104b7a733298be0
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-17 10:12:05 +0530

    Created the DbContext class (Data folder)

---
+++ VelvetVeogue/Data/AppDb.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VelvetVeogue.Models;
+
+namespace VelvetVeogue.Data
+{
+    public class AppDb: DbContext
+    {
+
+        public AppDb(DbContextOptions<AppDb> options ) : base (options)
+        {
+
+        }
+
+        public DbSet<TblLogin> TblLogins { get; set; }
+
+        public DbSet<Tbl_ItemDetails> Tbl_ItemDetails { get; set; }
+
+        public DbSet<TblContactUS> Tbl_ContactUS { get; set; }
+
+    }
+}

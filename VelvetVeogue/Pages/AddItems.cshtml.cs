5484df1a1c19f804bf9786f35476d912377bc2f0
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-18 16:14:19 +0530

    Backend validation of the Add Items page | status: completed

--- VelvetVeogue/Pages/AddItems.cshtml.cs
+++ VelvetVeogue/Pages/AddItems.cshtml.cs
@@ -1,10 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class AddItemsModel : PageModel
     {
+        /// <summary>
+        /// Variables
+        /// </summary>
+        [BindProperty]
+        [Required(ErrorMessage ="Please type the Category Name")]
+        public string tbxCategoryName { get; set; }
+
+        [BindProperty]
+        public string tbxName { get; set; }
+
+        [BindProperty,Required(ErrorMessage ="Please enter the color")]
+        public string tbxcolor { get; set; }
+
+        [BindProperty,Required(ErrorMessage ="Please enter the sizes")]
+        public string tbxsizes { get; set; }
+
+        [BindProperty,Required(ErrorMessage ="Please enter the Available Qty")]
+        public int? tbxAvailableQTY { get; set; }
+
+
+        [BindProperty, Required(ErrorMessage = "Please enter the Price")]
+        public double? tbxprice { get; set; }
+
+
+        /// <summary>
+        /// Dependency injectios
+        /// </summary>
+        private readonly AppDb _AppDb;
+
+        public AddItemsModel(AppDb appDb)
+        {
+            _AppDb = appDb;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+
+            return Page();
+        }
+
         public void OnGet()
         {
         }

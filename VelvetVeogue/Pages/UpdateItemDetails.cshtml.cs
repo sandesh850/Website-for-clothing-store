0e9a5ea3cfdb46970a9649e05912a04548146415
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-22 11:11:16 +0530

    loading details into update details page | status: success

--- VelvetVeogue/Pages/UpdateItemDetails.cshtml.cs
+++ VelvetVeogue/Pages/UpdateItemDetails.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class UpdateItemDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDb _appDb;
+
+        public UpdateItemDetailsModel(AppDb appDb)
         {
+            _appDb = appDb;
         }
+
+        [BindProperty]
+        public Tbl_ItemDetails? ItemDetails { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            ItemDetails = await _appDb.Tbl_ItemDetails.FindAsync(id);
+
+            return Page();
+        }
+
+        //public void OnGet()
+        //{
+        //}
     }
 }

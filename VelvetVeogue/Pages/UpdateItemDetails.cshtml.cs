cec5d7838ad7ae8f2228f9862a52c4d0acab067d
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-23 11:27:07 +0530

    Updating item details | status: success

--- VelvetVeogue/Pages/UpdateItemDetails.cshtml.cs
+++ VelvetVeogue/Pages/UpdateItemDetails.cshtml.cs
@@ -17,11 +17,45 @@ namespace VelvetVeogue.Pages
         [BindProperty]
         public Tbl_ItemDetails? ItemDetails { get; set; }
 
+        // Use to find selected record
         public async Task<IActionResult> OnGetAsync(int id)
         {
             ItemDetails = await _appDb.Tbl_ItemDetails.FindAsync(id);
 
             return Page();
+        }
+
+        // Updating record
+        public Tbl_ItemDetails? Existing_ItemDetails { get; set; }
+
+        public IFormFile? NewImage { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            // Retrieving existing items
+            Existing_ItemDetails = await _appDb.Tbl_ItemDetails.FindAsync(id);
+
+           
+            Existing_ItemDetails.CategoryName = ItemDetails.CategoryName;
+            Existing_ItemDetails.Name = ItemDetails.Name;
+            Existing_ItemDetails.Color = ItemDetails.Color;
+            Existing_ItemDetails.size = ItemDetails.size;
+            Existing_ItemDetails.AvailableQty = ItemDetails.AvailableQty;
+            Existing_ItemDetails.Price = ItemDetails.Price;
+
+            // updating image
+            if(NewImage != null && NewImage.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    await NewImage.CopyToAsync(ms);
+                    Existing_ItemDetails.img = ms.ToArray();
+                }
+            }
+          
+            await _appDb.SaveChangesAsync();
+
+            return RedirectToPage("/UpdateOrRemove");
         }
 
         //public void OnGet()

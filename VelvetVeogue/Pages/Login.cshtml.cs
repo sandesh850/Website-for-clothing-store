55920f11d5ffd43d49f5932d107adc9316df0288
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-23 16:57:41 +0530

    Login | status: completed | Logout not included

--- VelvetVeogue/Pages/Login.cshtml.cs
+++ VelvetVeogue/Pages/Login.cshtml.cs
@@ -1,12 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty,Required(ErrorMessage ="Please enter the username")]
+        public string tbxusername { get; set; }
+
+        [BindProperty,Required(ErrorMessage ="Please enter the password")]
+        public string tbxpassword { get; set; }
+
+        public readonly AppDb _appDb;
+
+        public LoginModel(AppDb appDb)
         {
+            _appDb = appDb;
         }
+
+        public List<TblLogin>? TblLogin { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            string existing_username = "";
+            string existing_password = "";
+
+            TblLogin = _appDb.TblLogins.ToList();
+
+            if(TblLogin != null)
+            {
+                existing_username = TblLogin[0].Username;
+                existing_password = TblLogin[0].Password;
+            }
+
+           
+
+            if(existing_username == tbxusername && existing_password == tbxpassword)
+            {
+                HttpContext.Session.SetString("Login","true");
+                return RedirectToPage("/index");
+            }
+            else
+            {
+                return Page();
+            }
+
+        }
+
+        //public void OnGet()
+        //{
+        //}
     }
 }
--- VelvetVeogue/Program.cs
+++ VelvetVeogue/Program.cs
@@ -6,11 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// My code || use to hanlen login authentication of sign up page
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+
 // My code || 
 builder.Services.AddDbContext<AppDb>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// My code ||  Enabling session before app.UseEndpoints
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())

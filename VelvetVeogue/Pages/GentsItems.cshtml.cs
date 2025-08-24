060dfe00884245cea5569ac74eeb694e640c7485
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-24 10:23:43 +0530

    Gents item page backend | status: not completed (75% completed)

--- VelvetVeogue/Pages/GentsItems.cshtml.cs
+++ VelvetVeogue/Pages/GentsItems.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class GentsItemsModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDb _appDb;
+
+        public GentsItemsModel(AppDb appDb)
         {
+            _appDb = appDb;
+        }
+
+       
+        public string CategoryName { get; set; }
+
+        public List<Tbl_ItemDetails> Tbl_ItemDetails { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Tbl_ItemDetails = _appDb.Tbl_ItemDetails.ToList();
+
+            CategoryName = Tbl_ItemDetails[0].CategoryName;
+
+            return Page();
         }
     }
 }

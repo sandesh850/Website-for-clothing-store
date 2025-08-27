23aeedf8d6c5a7f788d67171911d2ae320e766a9
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-27 20:36:00 +0530

    Inquiry page backend is completed

--- VelvetVeogue/Pages/Inquiries.cshtml.cs
+++ VelvetVeogue/Pages/Inquiries.cshtml.cs
@@ -1,12 +1,52 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class InquiriesModel : PageModel
     {
-        public void OnGet()
+        [BindProperty, Required(ErrorMessage = "Please Type the Message")]
+        public string tbxSubject { get; set; } = string.Empty;
+
+        [BindProperty, Required(ErrorMessage = "Please Type the Message")]
+        public string tbxMessage { get; set; } = string.Empty;
+
+
+
+        private readonly AppDb _appDb;
+
+        public InquiriesModel(AppDb appDb)
         {
+            _appDb = appDb;
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var TblInquiries = new Tbl_Inquiries
+                {
+                    sibject = tbxSubject,
+                    Message = tbxMessage
+                   
+                };
+
+                _appDb.Tbl_Inquiries.Add(TblInquiries);
+                await _appDb.SaveChangesAsync();
+
+                return RedirectToPage("index");
+
+            }
+
+            return Page();
+
+        }
+
+        //public void OnGet()
+        //{
+        //}
     }
 }

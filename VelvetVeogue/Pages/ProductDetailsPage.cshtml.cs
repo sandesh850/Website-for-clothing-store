2beea060c1d9fbd8772ca4a27148fe4e296ccb8f
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-28 10:03:12 +0530

    Backend of PDP || status: not complete

--- VelvetVeogue/Pages/ProductDetailsPage.cshtml.cs
+++ VelvetVeogue/Pages/ProductDetailsPage.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class ProductDetailsPageModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDb _appDb;
+
+        public ProductDetailsPageModel(AppDb appDb)
         {
+            _appDb = appDb;
         }
+
+        public List<Tbl_ItemDetails>? Tbl_ItemDetails { get; set; }
+
+        public Task<IActionResult> OnGetAsync()
+        {
+
+        }
+
+        //public void OnGet()
+        //{
+        //}
     }
 }

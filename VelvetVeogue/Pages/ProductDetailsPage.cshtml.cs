163614cce8d22db384c2a77c8cfc808be13251f1
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-08-28 21:37:04 +0530

    Modification in PDP | status: not complete

--- VelvetVeogue/Pages/ProductDetailsPage.cshtml.cs
+++ VelvetVeogue/Pages/ProductDetailsPage.cshtml.cs
@@ -14,12 +14,36 @@ namespace VelvetVeogue.Pages
             _appDb = appDb;
         }
 
-        public List<Tbl_ItemDetails>? Tbl_ItemDetails { get; set; }
+        [BindProperty]
+        public Tbl_ItemDetails? Tbl_ItemDetails { get; set; }
 
-        //public Task<IActionResult> OnGetAsync()
-        //{
+        [BindProperty]
+        public string sizes { get; set; }
 
-        //}
+        [BindProperty]
+        public string represent_sizes { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Tbl_ItemDetails = await _appDb.Tbl_ItemDetails.FindAsync(id);
+
+            // Split by comma and trim spaces
+            sizes = Tbl_ItemDetails.size;
+
+            var filterSizes = sizes.Split(',').Select(s => s.Trim()).ToList();
+
+            foreach(var filterSize in filterSizes)
+            {
+                if(filterSize == "S")
+                {
+                    represent_sizes = "S";
+                }
+            }
+
+
+
+            return Page();
+        }
 
         //public void OnGet()
         //{

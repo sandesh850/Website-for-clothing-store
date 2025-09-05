9d87ac91c7eaa98d7a1f1f8c122b910ea05802aa
Ravidu Sandesh <sandeshpiumantha52@gmail.com>
2025-09-05 10:42:03 +0530

    Page: login configurations | Description: backend | status: Not complete

--- VelvetVeogue/Pages/LoginConfig.cshtml.cs
+++ VelvetVeogue/Pages/LoginConfig.cshtml.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using VelvetVeogue.Data;
+using VelvetVeogue.Models;
 
 namespace VelvetVeogue.Pages
 {
     public class LoginConfigModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDb _appDb;
+
+        public LoginConfigModel(AppDb appDb)
         {
+            _appDb = appDb;
+        }
+
+
+        [BindProperty,Required(ErrorMessage ="Please enter the existing username")]
+        public string? tbxCurrent_username { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "Please enter the existing password")]
+        public string? tbxCurrent_pw { get; set; }
+
+
+        [BindProperty, Required(ErrorMessage = "Please enter the new username")]
+        public string? tbxNew_username { get; set; }
+
+        [BindProperty, Required(ErrorMessage = "Please enter the new password")]
+        public string? tbxNew_pw { get; set; }
+
+        public TblLogin? TblLogin { get; set; }
+
+
+
+        public IActionResult OnPost()
+        {
+            TblLogin = _appDb.TblLogins.Find(tbxCurrent_username);
+
+            return Page();
         }
     }
 }
